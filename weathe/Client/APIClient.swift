7fd9b83f45be560f787b74fa9924459617dc1765
Alex Rudoi <sanikbrant@gmail.com>
2020-03-12 12:12:41 +0200

    Created new personal lets for values

--- weathe/Client/APIClient.swift
+++ weathe/Client/APIClient.swift
@@ -23,23 +23,29 @@ struct ApiClient {
             let data = try? Data(contentsOf: URL(fileURLWithPath: file), options: []),
             let weather = try? JSONDecoder().decode(Forcast.self, from: data) else { return }
         
-        let temp = Int(weather.currently.apparentTemperature)
+        let city = weather.timezone
+        let temp = String("\(Int(weather.currently.apparentTemperature))º")
+        let weatherIcon = UIImage(named: weather.currently.icon)
+        let weatherDescription = weather.currently.summary
         let highTemp = Int(weather.daily.data.first!.temperatureHigh)
         let lowTemp = Int(weather.daily.data.first!.temperatureLow)
-        let chanceOfRain = Int(weather.currently.precipProbability*10)
-        let windSpeed = Int(weather.currently.windSpeed)
-        let humidity = Int(weather.currently.humidity*10)
+        let analyzeOfTemp = "\(highTemp)º / \(lowTemp)º"
+        let chanceOfRain = "Rain \(Int(weather.currently.precipProbability*10))%"
+        let windSpeed = "\(Int(weather.currently.windSpeed)) MPH"
+        let humidity = "\(Int(weather.currently.humidity*10))%"
+        let uxindex = String(weather.currently.uvIndex)
+        let totallyDescription = "Today: \(weather.daily.data.first!.summary)"
         
-        weatherViewController.cityName.text = weather.timezone
-        weatherViewController.cityTemp.text = String("\(temp)º")
-        weatherViewController.weatherIcon.image = UIImage(named: weather.currently.icon)
-        weatherViewController.weatherDescription.text = weather.currently.summary
-        weatherViewController.highLowTemp.text = "\(highTemp)º / \(lowTemp)º"
-        weatherViewController.percentRain.text = "Rain \(chanceOfRain)%"
-        weatherViewController.windSpeed.text = "\(windSpeed) MPH"
-        weatherViewController.humidity.text = "\(humidity)%"
-        weatherViewController.uvindex.text = String(weather.currently.uvIndex)
-        weatherViewController.totallyDescription.text = "Today: \(weather.daily.data.first!.summary)"
+        weatherViewController.cityName.text = city
+        weatherViewController.cityTemp.text = temp
+        weatherViewController.weatherIcon.image = weatherIcon
+        weatherViewController.weatherDescription.text = weatherDescription
+        weatherViewController.highLowTemp.text = analyzeOfTemp
+        weatherViewController.percentRain.text = chanceOfRain
+        weatherViewController.windSpeed.text = windSpeed
+        weatherViewController.humidity.text = humidity
+        weatherViewController.uvindex.text = uxindex
+        weatherViewController.totallyDescription.text = totallyDescription
         
         print(weather.currently)
     }
